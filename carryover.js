c71b1ece5c66c8dcee4e5d569cb4130926ac667b
logichulk <tusharsikand@gmail.com>
2019-08-03 23:30:24 +0530

    Create carryover.js

---
+++ carryover.js
@@ -0,0 +1,34 @@
+var prefix = "https://cosplayerashley.blogspot.com/p/";
+var url = window.location.href;
+alert(url);
+
+if (typeof handle === 'undefined') 
+{
+     var handle = "";
+    
+     if(url.indexof(prefix) > -1)
+     {
+          handle = url.replace(prefix, "").replace(".html", "");
+     }
+}
+
+var source = document.getElementsByTagName('html')[0].innerHTML;
+var foundIndex = source.indexOf("favourite Cosplayer now in a short story!");
+
+if(foundIndex > -1)
+{
+	handle = url.replace(prefix, "").replace(".html", "");
+
+	nextlink1 = document.getElementById("nextlink1").href;
+	nextlink2 = document.getElementById("nextlink2").href;
+
+	i(nextlink1 != null)
+	{
+		nextlink1.href = nextlink1.href.replace("${handle}", handle);
+	}
+
+	i(nextlink2 != null)
+	{
+		nextlink2.href = nextlink2.href.replace("${handle}", handle);
+	}
+}
