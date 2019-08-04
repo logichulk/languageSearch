7bf176f7e81f9e61676d08b507926fb73338b4c9
logichulk <tusharsikand@gmail.com>
2019-08-04 10:03:01 +0530

    Update profile.pic.js

--- profile.pic.js
+++ profile.pic.js
@@ -3,5 +3,9 @@ var url = "https://www.instagram.com/" + handle + "/?__a=1";
 $.getJSON(url, function(data) 
 {
     var profile_picture = document.getElementById("profile_picture");
-    profile_picture.innerHTML = "<img src='" + data.data.profile_picture + "' />";
+    
+    if(profile_picture != null)
+    {
+        profile_picture.innerHTML = "<img src='" + data.data.profile_picture + "' />";
+    }
 });
